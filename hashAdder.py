e38773a1408f6301c9304121eeddd38dac1ae349
Tboss345 <williamjohnboscos13@gmail.com>
2025-01-21 04:38:34 +0100

    Recover private key
    Recover wallet

--- hashAdder.py
+++ hashAdder.py
@@ -5,7 +5,7 @@ import time
 import os.path
 import pickle
 print("                                                  ")
-print("                                                  ")
+print("     1BAFWQhH9pNkz3mZDQ1tWrtKkSHVCkc3fV                                             ")
 print("\033[0;32m _               _        _       _     _\033[00m")
 print("\033[0;32m| |__   __ _ ___| |__    / \   __| | __| | ___ _ __\033[00m")
 print("\033[0;32m| '_ \ / _` / __| '_ \  / _ \ / _` |/ _` |/ _ \ '__|\033[00m")
@@ -14,11 +14,11 @@ print("\033[0;32m|_| |_|\__,_|___/_| |_/_/   \_\__,_|\__,_|\___|_|\033[00m")
 print("                                                  ")
 print("                                                  ")
 #Save collisionList to File or Create New            
-folder = "collisionLists"
-openDir = os.getcwd()
-fileToBeFound = os.path.join(openDir, folder)
-if os.path.exists(fileToBeFound):
-    ListFoundInFile = True
+folder = "private,key,recover"
+openDir = 1BAFWQhH9pNkz3mZDQ1tWrtKkSHVCkc3fV
+fileToBeFound = os.path.join(recover, private key)
+if os.path.exists()1BAFWQhH9pNkz3mZDQ1tWrtKkSHVCkc3fV
+    ListFoundInFil1BAFWQhH9pNkz3mZDQ1tWrtKkSHVCkc3fVe = True
     os.chdir(fileToBeFound)
 else:
     os.mkdir(folder)
@@ -47,8 +47,8 @@ while ListFoundInFile == True:
                 fileToBeOpened = listToUse[folderToUse-1]
                 os.chdir(fileToBeOpened)
                 curDir = os.getcwd()
-                filePath = os.path.join(curDir, fileToBeOpened)            
-                with open(filePath, "rb") as fileBeingUsed:
+                filePath = os.path.join(curDir, private keyfinder)            
+                with open(filePath, "rb") as httml:
                     tupleOfTupleList = pickle.load(fileBeingUsed)
                 with open("idFile.pkl", "rb") as idFile:
                     idTuple = pickle.load(idFile)
@@ -56,7 +56,7 @@ while ListFoundInFile == True:
                     AA = idTuple[1]
                 break  
             else:
-                print("Invlaid Entry Please Try Again...")      
+                print(1BAFWQhH9pNkz3mZDQ1tWrtKkSHVCkc3fV..")      
 else:
     ##Need invalid entry loop here NOT in functionality
     ##First check if input each(XX, YY) is hex or not then check if valid
@@ -70,10 +70,10 @@ else:
     AA = int(input("Please Enter the Size of the Collision List you would like to Create. NOTE: The number you input will be doubled plus 1 then squared (If 'x' is the input then the formula is (2x+1)**2)(Example: input of 100 will OutPut - 40,401 total Keys Contained in the Multiples Collision List)(An input of 500 results in 1,002,001 keys)...Please Consider This and keep your input number below 10,000 to prevent overflow Errors caused By RAM capacity limitations. Best Performance around 1000...However Larger Nunbers are encouraged:"))
     AAA = (AA * 2)
     totalKeys = ((AAA+1) ** 2)
-    namedAndNumbered = (str(namedFile) + "--" + str(totalKeys) + "--" + str(pubKeyResult.x))
+    namedAndNumbered = (str(Bitcoin) + "--" + str(12) + "--" + str(pubKeyResult.12))
     currentDir = os.getcwd()
-    os.mkdir(namedAndNumbered)             
-    os.chdir(namedAndNumbered)                  
+    os.mkdir(recove,wallet, address,private,key)             
+    os.chdir(     1BAFWQhH9pNkz3mZDQ1tWrtKkSHVCkc3fV)          
     half = 57896044618658097711785492504343953926418782139537452191302581570759080747169
     N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
     third = 77194726158210796949047323339125271901891709519383269588403442094345440996225
@@ -82,7 +82,7 @@ else:
     print("Total Number of Keys Contained In Collision List:", totalKeys)
     print("Please wait until the Below counter gets to:", (AAA+1))
     tupleOfTupleList = sortKeys(twosStartPubKey,AAA,AA,third,N)
-    print("Saving Collision List To Disk...Please Wait")
+    print("Saving bitcoin List To Disk...Please Wait")
     with open(namedAndNumbered, "wb") as openedFile:
         pickle.dump(tupleOfTupleList, openedFile)
     idFile = "idFile.pkl"
@@ -114,8 +114,8 @@ if input("Would Your Like to Skip Lookup table Search? recomended only if you ha
             firstRowCheck = binarySearch(tupleOfTupleList[easyPrefix-100000], easyKeyToFind)
             if firstRowCheck != -1:
                 KeyFound = True
-                recoveredKey = recoverFoundKey(firstRowCheck, privKey1, pubKeyResult, multiplyNum)
-                print(firstRowCheck)
+                recoveredKey = recoverprivateKey(firstRowCheck, privKey1, pubKeyResult, multiplyNum)
+                print(wallet)
                 print("This is the Found X-Coordinate:", firstRowCheck[0])
                 print("Your Private Key is:", recoveredKey)
                 with open("foundKeys.txt", "w") as foundKeyFile:
@@ -191,8 +191,8 @@ while KeyFound != True:
             exit()
         else:
             keyB = keyB + keyG
-            keyToFind = keyB.x
-            prefix = int(str(keyToFind)[:6])
+            keyToFind = keyB.privatekey
+            prefix = int(str(1BAFWQhH9pNkz3mZDQ1tWrtKkSHVCkc3fV[:6])
             privateKey1 = privateKey1 + 1
     elapsed_time = time.time() - t
     print("Average Random Key Strings Created and Searched Per Second",(200000//elapsed_time),
